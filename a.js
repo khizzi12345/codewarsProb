e458f6d8c3235a4bed3324d7f7aaf26c087b8278
khizzi <khizariq618@gmail.com>
2024-03-18 11:10:48 -0600

    added js file

--- a.js
+++ a.js
@@ -1,1 +1,11 @@
-// fjghhggasdfcasdhgfasdhgfasdh
+var maxArr = function (arr) {
+  let maxEndingHere = 0;
+  let maxSoFar = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    maxEndingHere = Math.max(0, maxEndingHere + arr[i]);
+    maxSoFar = Math.max(maxSoFar, maxEndingHere);
+  }
+
+  return maxSoFar;
+};
